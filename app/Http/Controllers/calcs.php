41a6a6fa403c6db2c46f5ab60155db1d420bd808
TakayukiKomada <Takakoma.79@gmail.com>
2022-10-14 06:57:32 +0900

    フォーマット修正

--- app/Http/Controllers/calcs.php
+++ app/Http/Controllers/calcs.php
@@ -6,25 +6,25 @@ use Illuminate\Http\Request;
 
 class calcs extends Controller
 {
-    public function result($num1,$operator, $num2)
+    public function result($num1, $operator, $num2)
     {
         switch ($operator) {
-    case 'addition':
-        $answer = $num1 + $num2;
-        break;
-    case 'subtraction':
-        $answer = $num1 - $num2;
-        break;
-    case 'multiplication':
-        $answer = $num1 * $num2;
-        break;
-    case 'division':
-        $answer = $num1 / $num2;
-        break;
-    default:
-        echo "正しい演算子を指定して下さい";
-        break;
-}
+            case 'addition':
+                $answer = $num1 + $num2;
+                break;
+            case 'subtraction':
+                $answer = $num1 - $num2;
+                break;
+            case 'multiplication':
+                $answer = $num1 * $num2;
+                break;
+            case 'division':
+                $answer = $num1 / $num2;
+                break;
+            default:
+                echo "正しい演算子を指定して下さい";
+                break;
+        }
         return view('message.result', ['answer' => $answer]);
     }
 }
